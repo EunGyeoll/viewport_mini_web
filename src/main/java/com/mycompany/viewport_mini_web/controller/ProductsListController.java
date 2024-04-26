481f6ef251e3cdbb8844d0a6334e925555f63571
Sunghwan Kwon <sunghwan.ian.kwon@gmail.com>
2024-04-26 17:33:51 +0900

    updated dtos

--- src/main/java/com/mycompany/viewport_mini_web/controller/ProductsListController.java
+++ src/main/java/com/mycompany/viewport_mini_web/controller/ProductsListController.java
@@ -1,6 +1,7 @@
 package com.mycompany.viewport_mini_web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.extern.slf4j.Slf4j;
 
@@ -16,7 +17,7 @@ public class ProductsListController {
     return "products/productsList";
   }
 
-  @RequestMapping("/productDetail")
+  @GetMapping("/productDetail")
   public String productDetail() {
     log.info("productDetail() 실행");
     return "products/productDetail";

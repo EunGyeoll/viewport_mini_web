018aacde03291e651cbfb5c49d3e58a5d8b98b35
mingyeongwon <wonmingy@gmail.com>
2024-05-03 21:15:46 +0900

    controller, service 수정

--- src/main/java/com/mycompany/viewport_mini_web/dto/Photos.java
+++ src/main/java/com/mycompany/viewport_mini_web/dto/Photos.java
@@ -1,5 +1,7 @@
 package com.mycompany.viewport_mini_web.dto;
 
+import java.util.List;
+
 import org.springframework.web.multipart.MultipartFile;
 
 import lombok.Data;
@@ -12,5 +14,5 @@ public class Photos {
   private String pattachtype;
   private byte[] pattachdata;
   
-  private MultipartFile pattach;
+  private List<MultipartFile> pattach;
 }
--- src/main/java/com/mycompany/viewport_mini_web/service/ProductService.java
+++ src/main/java/com/mycompany/viewport_mini_web/service/ProductService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.mycompany.viewport_mini_web.dao.PhotosDao;
 import com.mycompany.viewport_mini_web.dao.ProductDao;
